015eb82a69cf32aef8a22f4913caa51569ea332b
Mari <mari_firex@mail.ru>
2018-05-03 13:29:34 +0300

    Добавила клавиатуру

---
+++ KnitSencei_bot/KnitSencei_bot/Keyboard.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.InlineKeyboardButtons;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace KnitSencei_bot
+{
+    class Keyboard
+    {
+        public async void Start(TelegramBotClient Bot, MessageEventArgs e)
+        {
+            string text =
+ @"Список команд :
+/start - запуск бота,
+/inline - связь с разработчиком,
+/keyboard - вывод клавиатуры,
+/help - просить о помощи Сенсея,
+/calculator - вызвать калькулятор количества пряжи
+/ideas- показать идеи для вязания";
+            await Bot.SendTextMessageAsync(e.Message.From.Id, text);
+        }
+
+        public async void Inline(TelegramBotClient Bot, MessageEventArgs e)
+        {
+            var inlineKeyboard = new InlineKeyboardMarkup
+                    (new[]
+                    {new[]
+                        {
+                            InlineKeyboardButton.WithUrl("VK", "https://vk.com/id10618217"),
+                            InlineKeyboardButton.WithUrl("Telegram","https://t.me/litvinesha")
+                        },
+                        new []
+                        {
+                            InlineKeyboardButton.WithUrl("Google+", "https://plus.google.com/109645919916102660305"),
+                        }
+                    });
+            await Bot.SendTextMessageAsync(e.Message.From.Id, "Выбери пункт меню", replyMarkup: inlineKeyboard);
+        }
+
+        public async void Keyboard_k(TelegramBotClient Bot, MessageEventArgs e)
+        {
+            var replyKeyboard = new ReplyKeyboardMarkup(new[]
+                    {
+                        new []
+                        {
+                            new KeyboardButton("Ты кто?"),
+                            new KeyboardButton("Привет!")
+                        },
+                        new []
+                        {
+                            new KeyboardButton("Помоги мне!"),
+                            new KeyboardButton("Как дела?")
+                        }
+                    });
+            replyKeyboard.ResizeKeyboard = true;
+            await Bot.SendTextMessageAsync(e.Message.Chat.Id, "А вот и Я!", replyMarkup: replyKeyboard);
+        }
+
+        public async void Help(TelegramBotClient Bot, MessageEventArgs e)
+        {
+            var inlineKeyboard2 = new InlineKeyboardMarkup
+                  (new[] {
+                            InlineKeyboardButton.WithCallbackData("Новичок"),
+                            InlineKeyboardButton.WithCallbackData("Профессионал")
+                         });
+            await Bot.SendTextMessageAsync(e.Message.From.Id, "Выбери уровень своего мастерства", replyMarkup: inlineKeyboard2);
+        }
+
+        public async void Idea(TelegramBotClient Bot, MessageEventArgs e)
+        {
+            var inlineKeyboard3 = new InlineKeyboardMarkup
+                  (new[] {
+                            InlineKeyboardButton.WithCallbackData("Платья"),
+                            InlineKeyboardButton.WithCallbackData("Свитера")
+                         });
+            await Bot.SendTextMessageAsync(e.Message.From.Id, "Выбери, какие идеи тебя интересуют", replyMarkup: inlineKeyboard3);
+        }
+
+        public async void Calculator(TelegramBotClient Bot, MessageEventArgs e)
+        {
+            var inlineKeyboard4 = new InlineKeyboardMarkup(new[]
+                    {
+                        new []
+                        {
+                            InlineKeyboardButton.WithCallbackData("Указать данные")
+                        },
+                        new []
+                        {
+                            InlineKeyboardButton.WithCallbackData("Произвести расчет")
+                        }
+                    });
+            await Bot.SendTextMessageAsync(e.Message.From.Id, "Выбери, что ты хочешь сделать", replyMarkup: inlineKeyboard4);
+        }
+    }
+}
--- KnitSencei_bot/KnitSencei_bot/Program.cs
+++ KnitSencei_bot/KnitSencei_bot/Program.cs
@@ -20,6 +20,9 @@ namespace KnitSencei_bot
     {
         static TelegramBotClient Bot;
         static ApiAi apiAi;
+        static FillIProduct fill_product = new FillIProduct();
+        static List<DataOfProduct> product = new List<DataOfProduct>();
+        static Keyboard keyboard = new Keyboard();
 
         static void Main(string[] args)
         {
@@ -28,153 +31,58 @@ namespace KnitSencei_bot
             AIConfiguration config = new AIConfiguration("cee1d885245c490a85cf0ce0ef756cc4", SupportedLanguage.Russian);
             apiAi = new ApiAi(config);
 
-
             Bot.OnMessage += Bot_OnMessageReceived;
             Bot.OnCallbackQuery += Bot_OnCallbackQueryReceived;
 
-            var me = Bot.GetMeAsync().Result;
-            Console.WriteLine(me.FirstName);
             Bot.StartReceiving();
+            fill_product.Fill_product(product); //заполнение листа информацией из тхт
             Console.ReadLine();
             Bot.StopReceiving();
         }
 
         
-        private static async void Bot_OnCallbackQueryReceived(object sender, Telegram.Bot.Args.CallbackQueryEventArgs e)
+        private static async void Bot_OnCallbackQueryReceived(object sender, CallbackQueryEventArgs e)
         {
             string buttonText = e.CallbackQuery.Data;
             string name = $"{e.CallbackQuery.From.FirstName}{e.CallbackQuery.From.LastName}";
             Console.WriteLine($"{name} нажал(а) кнопку {buttonText}");
 
             if (buttonText == "Новичок")
-            {
                 await Bot.SendTextMessageAsync(e.CallbackQuery.From.Id, "Я с радостью научу тебя всему, что знаю сам!");
-
-            }
             else if (buttonText == "Профессионал")
-            {
                 await Bot.SendTextMessageAsync(e.CallbackQuery.From.Id, "Мои поздравления, коллега! Чем могу помочь?");
-            }
-
-
             else if (buttonText == "Указать данные")
-            {
                 await Bot.SendTextMessageAsync(e.CallbackQuery.From.Id, "Укажи вид изделия, размер и данные о пряже");
-            }
-
             else if (buttonText == "Произвести расчет")
-
-            {
+            { 
                 CalculationData cd = new CalculationData();
                 cd.datas("майка", "43", "67", "0");
-
                 await Bot.SendTextMessageAsync(e.CallbackQuery.From.Id, "Результат расчета - ");
             }
             await Bot.AnswerCallbackQueryAsync(e.CallbackQuery.Id, $"Ты нажал(а) кнопку {buttonText}");
         }
 
-        private static async void Bot_OnMessageReceived(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        private static void Bot_OnMessageReceived(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
             var message = e.Message;
             if (message == null || message.Type != MessageType.TextMessage)
-                return;
-                
+                Bot.SendTextMessageAsync(e.Message.Chat.Id, "Извини, но я могу воспринимать только текст");
             string name = $"{message.From.FirstName}{message.From.LastName}";
             Console.WriteLine($"{name} отправил(a) сообщение '{message.Text}'  ");
-           
             switch (message.Text)
             {
-                case "/start":
-                    string text =
- @"Список команд :
-/start - запуск бота,
-/inline - связь с разработчиком,
-/keyboard - вывод клавиатуры,
-/help - просить о помощи Сенсея,
-/calculator - вызвать калькулятор количества пряжи
-/ideas- показать идеи для вязания";
-                    await Bot.SendTextMessageAsync(message.From.Id, text);
-                    break;
-                case "/inline":
-                    var inlineKeyboard = new InlineKeyboardMarkup
-                    (new[]
-                    {new[]
-                        {
-                            InlineKeyboardButton.WithUrl("VK", "https://vk.com/id10618217"),
-                            InlineKeyboardButton.WithUrl("Telegram","https://t.me/litvinesha")
-                        },
-                        new []
-                        {
-                            InlineKeyboardButton.WithUrl("Google+", "https://plus.google.com/109645919916102660305"),
-                           
-                        }
-                    });
-                    await Bot.SendTextMessageAsync(message.From.Id, "Выбери пункт меню", replyMarkup:inlineKeyboard);
-                    break;
-                case "/keyboard":
-                    var replyKeyboard = new ReplyKeyboardMarkup(new[]
-                    {
-                        new []
-                        {
-                            new KeyboardButton("Ты кто?"),
-                            new KeyboardButton("Привет!")
-                        },
-                        new []
-                        {
-                            new KeyboardButton("Помоги мне!"),
-                            new KeyboardButton("Как дела?")
-                        },
-                        new [] 
-                        {
-                            new KeyboardButton("Контакт") {RequestContact = true},
-                            new KeyboardButton("Геолокация") {RequestLocation = true}
-                        }
-                    });
-                    await Bot.SendTextMessageAsync(message.Chat.Id, "А вот и Я!",
-                        replyMarkup: replyKeyboard);
-                    break;
-                case "/help":
-                    var inlineKeyboard2 = new InlineKeyboardMarkup
-                  (new[] {
-                            InlineKeyboardButton.WithCallbackData("Новичок"),
-                            InlineKeyboardButton.WithCallbackData("Профессионал")
-                         });
-                    await Bot.SendTextMessageAsync(message.From.Id, "Выбери уровень своего мастерства", replyMarkup: inlineKeyboard2);
-                    break;
-
-                case "/ideas":
-                    var inlineKeyboard3 = new InlineKeyboardMarkup
-                  (new[] {
-                            InlineKeyboardButton.WithCallbackData("Платья"),
-                            InlineKeyboardButton.WithCallbackData("Свитера")
-                         });
-                    await Bot.SendTextMessageAsync(message.From.Id, "Выбери, какие идеи тебя интересуют", replyMarkup: inlineKeyboard3);
-                    break;
-
-                case "/calculator":
-                    var inlineKeyboard4= new InlineKeyboardMarkup(new[]
-                    {
-                        new []
-                        {
-                            InlineKeyboardButton.WithCallbackData("Указать данные")
-                        },
-                        new []
-                        {
-                            InlineKeyboardButton.WithCallbackData("Произвести расчет")
-                        }
-                    });
-                
-                    await Bot.SendTextMessageAsync(message.From.Id, "Выбери, что ты хочешь сделать", replyMarkup: inlineKeyboard4);
-                    break;
-
-
+                case "/start": keyboard.Start(Bot, e); break;
+                case "/inline": keyboard.Inline(Bot, e); break;
+                case "/keyboard": keyboard.Keyboard_k(Bot, e); break;
+                case "/help": keyboard.Help(Bot, e); break;
+                case "/ideas": keyboard.Idea(Bot, e); break;
+                case "/calculator": keyboard.Calculator(Bot, e); break;
                 default:
-                    var response = apiAi.TextRequest(message.Text);
+                    /*var response = apiAi.TextRequest(message.Text);
                     string answer = response.Result.Fulfillment.Speech;
                     if (answer == "")
                         answer = "Прости, я не понимаю тебя";
-                    await Bot.SendTextMessageAsync(message.From.Id, answer);
-
+                    await Bot.SendTextMessageAsync(message.From.Id, answer);*/
                     break;
             }
         }
